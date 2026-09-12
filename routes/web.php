a25557fdff277775aae01d3891e07ab03a92aa5b
JoseZapata71 <josezapatagomez20000@gmail.com>
2026-09-12 05:04:05 -0600

    Configurar rutas web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('/app-ciudadano', 'ciudadano')->name('app.ciudadano');
+Route::view('/app-conductor', 'conductor')->name('app.conductor');
+Route::view('/app-admin', 'admin')->name('app.admin');
